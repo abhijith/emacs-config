38d39502659a88dda22cc152e85e604d8f5519d5
Abhijith <abhijithg@gmail.com>
2018-11-24 11:50:00 +0530

    enable go-eldoc minor mode

--- init-available/init-go.el
+++ init-available/init-go.el
@@ -1,10 +1,11 @@
 (require 'packages)
 
-;; go-autocomplete dependency -> $ go get -u github.com/nsf/gocode
+;; go-autocomplete dependency -> $ go get -u github.com/mdempsky/gocode
+
 ;; $ go get github.com/rogpeppe/godef
 ;; go imports ->  go get golang.org/x/tools/cmd/goimports
 
-(install-packages '(go-mode go-complete go-autocomplete))
+(install-packages '(go-mode go-complete go-autocomplete go-errcheck go-eldoc))
 
 (require 'go-mode)
 
@@ -22,15 +23,17 @@
   ; Godef jump key binding
   (local-set-key (kbd "M-.") 'godef-jump)
   (local-set-key (kbd "M-*") 'pop-tag-mark)
+  (local-set-key (kbd "C-c C-k") 'godoc)
   (add-hook 'before-save-hook 'gofmt-before-save)
   (setq tab-width 4 indent-tabs-mode 1))
-
-(add-hook 'go-mode-hook 'my-go-mode-hook)
 
 (defun auto-complete-for-go ()
   (auto-complete-mode 1))
 
-(add-hook 'go-mode-hook 'auto-complete-for-go)
+(require 'go-eldoc)
+(add-hook 'go-mode-hook 'go-eldoc-setup)
+
+(add-hook 'go-mode-hook 'my-go-mode-hook)
 
 (with-eval-after-load 'go-mode
   (require 'go-autocomplete)
