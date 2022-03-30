f00fc5a99da902fad30c2998cd4ef86ff3a0ed28
Abhijith Gopal <abhijithg@gmail.com>
2022-03-31 00:01:44 +0530

    wip: clj-mode

--- init-available/init-clj.el
+++ init-available/init-clj.el
@@ -1,91 +1,91 @@
 (use-package clojure-mode :straight t)
 (use-package cider :straight t)
 (use-package ac-cider :straight t)
-(use-package paredit :straight t)
+;; (use-package paredit :straight t)
 
-(defmacro defclojureface (name color desc &optional others)
-  `(defface ,name '((((class color)) (:foreground ,color ,@others))) ,desc :group 'faces))
+;; (defmacro defclojureface (name color desc &optional others)
+;;   `(defface ,name '((((class color)) (:foreground ,color ,@others))) ,desc :group 'faces))
 
-(defclojureface clojure-parens       "DimGrey"   "Clojure parens")
-(defclojureface clojure-braces       "DimGrey"   "Clojure braces")
-(defclojureface clojure-brackets     "DimGrey" "Clojure brackets")
-;;(defclojureface clojure-keyword      "OrangeRed4" "Clojure keywords")
-;;(defclojureface clojure-keyword      "tomato4" "Clojure keywords")
-(defclojureface clojure-keyword      "IndianRed4" "Clojure keywords")
+;; (defclojureface clojure-parens       "DimGrey"   "Clojure parens")
+;; (defclojureface clojure-braces       "DimGrey"   "Clojure braces")
+;; (defclojureface clojure-brackets     "DimGrey" "Clojure brackets")
+;; ;;(defclojureface clojure-keyword      "OrangeRed4" "Clojure keywords")
+;; ;;(defclojureface clojure-keyword      "tomato4" "Clojure keywords")
+;; (defclojureface clojure-keyword      "IndianRed4" "Clojure keywords")
 
-;; (defclojureface clojure-namespace    "#c476f1"   "Clojure namespace")
-;; (defclojureface clojure-java-call    "#4bcf68"   "Clojure Java calls")
-;; (defclojureface clojure-special      "#b8bb00"   "Clojure special")
-;; (defclojureface clojure-double-quote "#b8bb00"   "Clojure special" (:background "unspecified"))
+;; ;; (defclojureface clojure-namespace    "#c476f1"   "Clojure namespace")
+;; ;; (defclojureface clojure-java-call    "#4bcf68"   "Clojure Java calls")
+;; ;; (defclojureface clojure-special      "#b8bb00"   "Clojure special")
+;; ;; (defclojureface clojure-double-quote "#b8bb00"   "Clojure special" (:background "unspecified"))
 
-(defun tweak-clojure-syntax ()
-  (mapcar (lambda (x) (font-lock-add-keywords nil x))
-          '((("#?['`]*(\\|)"       . 'clojure-parens))
-            (("#?\\^?{\\|}"        . 'clojure-brackets))
-            (("\\[\\|\\]"          . 'clojure-braces))
-            ((":\\w+"              . 'clojure-keyword))
-            (("#?\""               0 'clojure-double-quote prepend))
-            (("nil\\|true\\|false\\|%[1-9]?" . 'clojure-special))
-            (("(\\(\\.[^ \n)]*\\|[^ \n)]+\\.\\|new\\)\\([ )\n]\\|$\\)" 1 'clojure-java-call)))))
+;; (defun tweak-clojure-syntax ()
+;;   (mapcar (lambda (x) (font-lock-add-keywords nil x))
+;;           '((("#?['`]*(\\|)"       . 'clojure-parens))
+;;             (("#?\\^?{\\|}"        . 'clojure-brackets))
+;;             (("\\[\\|\\]"          . 'clojure-braces))
+;;             ((":\\w+"              . 'clojure-keyword))
+;;             (("#?\""               0 'clojure-double-quote prepend))
+;;             (("nil\\|true\\|false\\|%[1-9]?" . 'clojure-special))
+;;             (("(\\(\\.[^ \n)]*\\|[^ \n)]+\\.\\|new\\)\\([ )\n]\\|$\\)" 1 'clojure-java-call)))))
 
-(add-hook 'clojure-mode-hook 'tweak-clojure-syntax)
+;; (add-hook 'clojure-mode-hook 'tweak-clojure-syntax)
 
-(defvar clojure-mode-font-lock-comment-sexp nil)
+;; (defvar clojure-mode-font-lock-comment-sexp nil)
 
-(defun clojure-font-lock-setup ()
-  (interactive)
-  (set (make-local-variable 'lisp-indent-function)
-       'clojure-indent-function)
-  (set (make-local-variable 'lisp-doc-string-elt-property)
-       'clojure-doc-string-elt)
-  (set (make-local-variable 'font-lock-multiline) t)
+;; (defun clojure-font-lock-setup ()
+;;   (interactive)
+;;   (set (make-local-variable 'lisp-indent-function)
+;;        'clojure-indent-function)
+;;   (set (make-local-variable 'lisp-doc-string-elt-property)
+;;        'clojure-doc-string-elt)
+;;   (set (make-local-variable 'font-lock-multiline) t)
 
-  (add-to-list 'font-lock-extend-region-functions
-               'clojure-font-lock-extend-region-def t)
+;;   (add-to-list 'font-lock-extend-region-functions
+;;                'clojure-font-lock-extend-region-def t)
 
-  (when clojure-mode-font-lock-comment-sexp
-    (add-to-list 'font-lock-extend-region-functions
-                 'clojure-font-lock-extend-region-comment t)
-    (make-local-variable 'clojure-font-lock-keywords)
-    (add-to-list 'clojure-font-lock-keywords
-                 'clojure-font-lock-mark-comment t)
-    (set (make-local-variable 'open-paren-in-column-0-is-defun-start) nil))
+;;   (when clojure-mode-font-lock-comment-sexp
+;;     (add-to-list 'font-lock-extend-region-functions
+;;                  'clojure-font-lock-extend-region-comment t)
+;;     (make-local-variable 'clojure-font-lock-keywords)
+;;     (add-to-list 'clojure-font-lock-keywords
+;;                  'clojure-font-lock-mark-comment t)
+;;     (set (make-local-variable 'open-paren-in-column-0-is-defun-start) nil))
 
-  (setq font-lock-defaults
-        '(clojure-font-lock-keywords    ; keywords
-          nil nil
-          (("+-*/.<>=!?$%_&~^:@" . "w")) ; syntax alist
-          nil
-          (font-lock-mark-block-function . mark-defun)
-          (font-lock-syntactic-face-function
-           . lisp-font-lock-syntactic-face-function))))
+;;   (setq font-lock-defaults
+;;         '(clojure-font-lock-keywords    ; keywords
+;;           nil nil
+;;           (("+-*/.<>=!?$%_&~^:@" . "w")) ; syntax alist
+;;           nil
+;;           (font-lock-mark-block-function . mark-defun)
+;;           (font-lock-syntactic-face-function
+;;            . lisp-font-lock-syntactic-face-function))))
 
-(eval-after-load 'grep
-  '(when (boundp 'grep-find-ignored-files)
-    (add-to-list 'grep-find-ignored-files "target")
-    (add-to-list 'grep-find-ignored-files "*.class")))
+;; (eval-after-load 'grep
+;;   '(when (boundp 'grep-find-ignored-files)
+;;     (add-to-list 'grep-find-ignored-files "target")
+;;     (add-to-list 'grep-find-ignored-files "*.class")))
 
-(use-package ac-cider)
-; (add-hook 'cider-mode-hook 'ac-flyspell-workaround)
+;; (use-package ac-cider)
+;; ; (add-hook 'cider-mode-hook 'ac-flyspell-workaround)
 
-(defun set-auto-complete-as-completion-at-point-function ()
-  (setq completion-at-point-functions '(auto-complete)))
+;; (defun set-auto-complete-as-completion-at-point-function ()
+;;   (setq completion-at-point-functions '(auto-complete)))
 
-;; (add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
-;; (add-hook 'cider-mode-hook 'set-auto-complete-as-completion-at-point-function)
-(add-hook 'cider-mode-hook 'eldoc-mode)
-(setq cider-repl-use-clojure-font-lock t)
+;; ;; (add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
+;; ;; (add-hook 'cider-mode-hook 'set-auto-complete-as-completion-at-point-function)
+;; (add-hook 'cider-mode-hook 'eldoc-mode)
+;; (setq cider-repl-use-clojure-font-lock t)
 
-;; ;; clojure stuff
-(use-package company :straight t)
-(add-hook 'cider-repl-mode-hook 'company-mode)
-(add-hook 'cider-mode-hook 'company-mode)
+;; ;; ;; clojure stuff
+;; (use-package company :straight t)
+;; (add-hook 'cider-repl-mode-hook 'company-mode)
+;; (add-hook 'cider-mode-hook 'company-mode)
 
-(defun xtra-cider-repl-bindings ()
-  (local-set-key (kbd "C-c C-l") 'cider-repl-clear-buffer))
+;; (defun xtra-cider-repl-bindings ()
+;;   (local-set-key (kbd "C-c C-l") 'cider-repl-clear-buffer))
 
-(add-hook 'cider-repl-mode-hook 'eldoc-mode)
-(add-hook 'cider-repl-mode-hook 'xtra-cider-repl-bindings)
+;; (add-hook 'cider-repl-mode-hook 'eldoc-mode)
+;; (add-hook 'cider-repl-mode-hook 'xtra-cider-repl-bindings)
 
 
 (provide 'init-clj)
