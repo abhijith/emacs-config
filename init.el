56ffe09b940a2f8b67c7aa92058a2daca6be2c62
Abhijith Gopal <abhijithg@gmail.com>
2022-04-01 20:49:31 +0530

    which-key + remove racer

--- init.el
+++ init.el
@@ -103,3 +103,14 @@
 
 (use-package rainbow-delimiters
   :straight t)
+
+(use-package which-key
+  :straight t
+
+  ;; Allow C-h to trigger which-key before it is done automatically
+  :config (setq which-key-show-early-on-C-h t)
+  ;; make sure which-key doesn't show normally but refreshes quickly after it is
+  ;; triggered.
+  (setq which-key-idle-delay 10000)
+  (setq which-key-idle-secondary-delay 0.05)
+  (which-key-mode))
