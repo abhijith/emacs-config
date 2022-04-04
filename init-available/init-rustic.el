355b3ef53af5fab1a409f56f906b10f106e6fd0a
Abhijith Gopal <abhijithg@gmail.com>
2022-04-04 18:40:46 +0530

    quiter eldoc

--- init-available/init-rustic.el
+++ init-available/init-rustic.el
@@ -75,7 +75,7 @@
   :custom
   ;; what to use when checking on-save. "check" is default, I prefer clippy
   ;; (lsp-rust-analyzer-cargo-watch-command "clippy")
-  ;; (lsp-eldoc-render-all t)
+  (lsp-eldoc-render-all nil)
   (lsp-idle-delay 1)
   (lsp-rust-analyzer-server-display-inlay-hints nil)
   :config
