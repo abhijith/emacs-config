5c2cf4d199d3aca88843a059f82785235f69a223
Abhijith <abhijithg@gmail.com>
2026-03-03 00:47:01 +0530

    Revert "cleanup + remove redundant lines"
    This reverts commit f7598306befb1e2b4e0cbb32a7b5ddba8f2a52e6.

--- init-available/init-rust-lsp.el
+++ init-available/init-rust-lsp.el
@@ -3,34 +3,38 @@
   :init
 
   (defun my-try-expand-lsp-smart (old)
-    "Succinct LSP expansion: 1 match = expand; >1 = minibuffer; 0 = fail."
-    (unless old
-      (let ((capf (run-hook-with-args-until-success 'completion-at-point-functions)))
-	(when (and (listp capf) (nth 2 capf))
-          (let* ((beg (nth 0 capf)) (end (nth 1 capf)) (table (nth 2 capf))
-		 (prefix (buffer-substring-no-properties beg end))
-		 (result (try-completion prefix table)))
-            (cond
-             ((stringp result) (completion-at-point)) ; Multiple matches -> UI
-             ((eq result t) (completion-at-point)) ; Unique match -> Expand
-             (t nil))))))			   ; No match -> Fail
-    nil) ; Always return nil to prevent hippie-expand from trying to "cycle"
+  "Succinct LSP expansion: 1 match = expand; >1 = minibuffer; 0 = fail."
+  (unless old
+    (let ((capf (run-hook-with-args-until-success 'completion-at-point-functions)))
+      (when (and (listp capf) (nth 2 capf))
+        (let* ((beg (nth 0 capf)) (end (nth 1 capf)) (table (nth 2 capf))
+               (prefix (buffer-substring-no-properties beg end))
+               (result (try-completion prefix table)))
+          (cond
+           ((stringp result) (completion-at-point)) ; Multiple matches -> UI
+           ((eq result t) (completion-at-point))    ; Unique match -> Expand
+           (t nil))))))                             ; No match -> Fail
+  nil) ; Always return nil to prevent hippie-expand from trying to "cycle"
 
 
   :hook ((rust-mode . lsp-deferred)
          (rust-mode . rainbow-delimiters-mode)
-	 (rust-mode . (lambda () (flymake-mode t)))
-	 (rust-mode . superword-mode))
+         (rust-mode . superword-mode))
 
-
+  :config
+  (setq rust-rustfmt-bin "rustup"
+	rust-format-on-save t
+        rust-rustfmt-switches '("run" "nightly" "rustfmt"))
+  (setq completion-category-overrides
+	'((lsp-capf (styles . (basic))))) ; 'basic' is prefix-only, no fuzzy.
   :bind (:map rust-mode-map
-	      ;; so that run C-c C-c C-r works without having to confirm, but don't try to
-	      ;; save rust buffers that are not file visiting. Once
-	      ;; https://github.com/brotzeit/rustic/issues/253 has been resolved this should
-	      ;; no longer be necessary.
+  ;; so that run C-c C-c C-r works without having to confirm, but don't try to
+  ;; save rust buffers that are not file visiting. Once
+  ;; https://github.com/brotzeit/rustic/issues/253 has been resolved this should
+  ;; no longer be necessary.
               ("M-n"   . flymake-goto-next-error)
               ("M-p"   . flymake-goto-prev-error)
-              ("C-c C-d" . lsp-describe-thing-at-point)
+              ("C-c C-d"   . lsp-describe-thing-at-point)
 
 	      ;; ("M-TAB" . completion-at-point)
               ("M-TAB" . hippie-expand)
@@ -45,24 +49,24 @@
               ("C-c C-c s" . lsp-rust-analyzer-status))
 
   :config
-  (setq rust-rustfmt-bin "rustup"
-	rust-format-on-save t
-        rust-rustfmt-switches '("run" "nightly" "rustfmt"))
 
-  (setq completion-category-overrides
-	'((lsp-capf (styles . (basic))))) ; 'basic' is prefix-only, no fuzzy.
+(add-hook 'rust-mode-hook
+          (lambda ()
+            ;; Set the list to EXACTLY one function.
+            ;; No filenames, no lines, no buffer text.
+            (setq-local hippie-expand-try-functions-list
+                        '(my-try-expand-lsp-smart))))
 
-
-  (setq-local hippie-expand-try-functions-list
-	      '(my-try-expand-lsp-smart))
-
+  ;; Optional: If you want it to fall back to buffer text if LSP fails:
   ;; (setq-local hippie-expand-try-functions-list '(my-try-expand-lsp try-expand-dabbrev))
-  (setq lsp-rust-analyzer-completion-postfix-enable nil)
-  (setq lsp-rust-analyzer-proc-macro-enable t)
-  ;; Also recommended: allow the LSP to run 'cargo check' for better errors
-  (setq lsp-rust-analyzer-cargo-load-out-dirs-from-check t)
+  (setq lsp-rust-analyzer-completion-postfix-enable nil) ; Disable postfix (e.g., .if -> if)
   (setq lsp-prefer-capf t)
-  (setq lsp-enable-symbol-highlighting t)
+  (setq lsp-enable-symbol-highlighting nil)
+  (setq lsp-signature-auto-activate nil)
+
+  ;; uncomment for plain lsp
+
+  (setq lsp-enable-symbol-highlighting nil)
 
   (setq lsp-ui-doc-enable nil)
   (setq lsp-ui-doc-show-with-cursor nil)
@@ -72,29 +76,33 @@
   (setq lsp-ui-sideline-show-code-actions nil)
   (setq lsp-ui-sideline-enable nil)
   (setq lsp-ui-sideline-show-hover nil)
+  (setq lsp-ui-sideline-enable nil)
   (setq lsp-ui-sideline-show-diagnostics nil)
 
   (setq lsp-lens-enable nil)
   (setq lsp-headerline-breadcrumb-enable nil)
 
-  (setq lsp-diagnostics-provider :flymake)
+  (setq lsp-diagnostics-provider :none)
 
-  (setq lsp-eldoc-enable-hover t)
+  (setq lsp-eldoc-enable-hover nil)
 
   (setq lsp-modeline-diagnostics-enable nil)
   (setq lsp-modeline-code-actions-enable nil)
-
   (setq lsp-rust-target-dir "~/.cargo/target")
+
 
   (setq lsp-signature-auto-activate nil) ;; you could manually request them via `lsp-signature-activate`
   (setq lsp-signature-render-documentation nil)
 
   (setq lsp-completion-provider :none)
-  (setq lsp-completion-show-detail t)
-  (setq lsp-completion-show-kind t)
+  (setq lsp-completion-show-detail nil)
+  (setq lsp-completion-show-kind nil)
 
   ;; comment to disable rustfmt on save
-  (setq rust-format-on-save t))
+  (setq rust-format-on-save t)
+  (setq lsp-rust-analyzer-proc-macro-enable nil)
+  (add-hook 'rust-mode-hook 'rainbow-delimiters-mode)
+  (add-hook 'rust-mode-hook #'(lambda () (flymake-mode nil))))
 
 
 (use-package lsp-mode
@@ -108,7 +116,7 @@
   (lsp-rust-analyzer-server-display-inlay-hints nil)
   :config
   (add-hook 'lsp-mode-hook 'lsp-ui-mode)
-  (add-hook 'lsp-mode-hook #'(lambda () (flymake-mode t))))
+  (add-hook 'lsp-mode-hook #'(lambda () (flymake-mode nil))))
 
 (use-package lsp-ui
   :straight t
